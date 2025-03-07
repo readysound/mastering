532b5e34d188ebec73c6c80a307f60bbdc355000
YourGitHubUsername <your-email@example.com>
2025-03-07 05:41:29 -0500

    Updated documentation and added new Markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'My Docs',
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: 'Introduction',
+                  autogenerate: { directory: 'introduction' },
+              },
+              {
+                  label: 'Guides',
+                  autogenerate: { directory: 'guides' },
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  adapter: vercel(),
 });
---
+++ src/content/docs/introduction/get started.md
@@ -0,0 +1,17 @@
+---
+title: Get Started
+description: A reference page in my new Starlight docs site.
+---
+
+Reference pages are ideal for outlining how things work in terse and clear terms.
+Less concerned with telling a story or addressing a specific use case, they should give a comprehensive outline of what you're documenting.
+
+## Further reading
+
+- Read [about reference](https://diataxis.fr/reference/) in the Diátaxis framework
+
+## Mastering Advice
+
+I think I am going to test few things such as this type of word's and also a word like sock and anna colosimo and also.
+
+Something like empanada de pollo.
